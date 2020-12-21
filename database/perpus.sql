05972c31f0f43a8e34e648ed9dfa2dbe30dc1d58
Olivia Widiyanti Saputri <oliviawidiyanti.saputri@gmail.com>
2020-12-21 15:57:43 +0700

    Create perpus.sql

---
+++ database/perpus.sql
@@ -0,0 +1,255 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Des 2020 pada 09.56
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 5.6.39
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(5) NOT NULL,
+  `nama_admin` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `username`, `password`) VALUES
+(15, 'oliviaws', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` int(5) NOT NULL,
+  `nama_anggota` varchar(45) NOT NULL,
+  `gender` enum('Laki-Laki','Perempuan') NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `gender`, `no_telp`, `alamat`, `email`, `password`) VALUES
+(1, 'Olivia Widiyanti Saputri', 'Perempuan', '083872425327', 'Jl. Muara Bahari', 'oliviawidiyanti.saputri@gmail.', 'ows191001');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(5) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
+  `judul_buku` varchar(50) NOT NULL,
+  `pengarang` varchar(35) NOT NULL,
+  `thn_terbit` date NOT NULL,
+  `penerbit` varchar(50) NOT NULL,
+  `isbn` varchar(25) NOT NULL,
+  `jumlah_buku` int(3) NOT NULL,
+  `lokasi` enum('Rak 1','Rak 2','Rak 3') NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tgl_input` date NOT NULL,
+  `status_buku` enum('1','0') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `id_kategori`, `judul_buku`, `pengarang`, `thn_terbit`, `penerbit`, `isbn`, `jumlah_buku`, `lokasi`, `gambar`, `tgl_input`, `status_buku`) VALUES
+(14, 0, 'pandangan pertama', 'aku', '2020-10-31', 'Gagas Media', '456789098765', 20, 'Rak 1', 'gambar1604974820.jpg', '2020-11-10', '1'),
+(15, 1, 'aku satu', 'cinta', '2020-12-10', 'loveble redaksi', '12345678910', 40, 'Rak 1', 'gambar1608482204.jpg', '0000-00-00', '1'),
+(16, 1, 'byebye', 'gvjxahkxjcl', '2020-11-25', 'lalayeye', '12345678910', 50, 'Rak 1', 'gambar1606284546.JPG', '0000-00-00', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pinjam`
+--
+
+CREATE TABLE `detail_pinjam` (
+  `id_pinjam` int(5) NOT NULL,
+  `id_buku` int(5) NOT NULL,
+  `tgl_pengembalian` date NOT NULL,
+  `denda` double NOT NULL,
+  `status_kembali` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'novel');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_pinjam` int(11) NOT NULL,
+  `id_anggota` int(4) NOT NULL,
+  `tanggal` date NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `totaldenda` double NOT NULL,
+  `status_pinjam` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_pinjam` int(11) NOT NULL,
+  `tgl_pencatatan` datetime NOT NULL,
+  `id_anggota` int(4) NOT NULL,
+  `id_buku` int(4) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `denda` double NOT NULL,
+  `tgl_pengembalian` date NOT NULL,
+  `total_denda` double NOT NULL,
+  `status_pengembalian` varchar(15) NOT NULL,
+  `status_peminjaman` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `detail_pinjam`
+--
+ALTER TABLE `detail_pinjam`
+  ADD UNIQUE KEY `id_pinjam` (`id_pinjam`,`id_buku`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pinjam`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_pinjam`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_anggota` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
